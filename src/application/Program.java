1bf6b9f6ed0cd8546c085bda5f2467170b492041
helbertms <helbertms@live.com>
2020-06-15 19:25:06 -0300

    Creating project and git repositoryDepartament entity class

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entites.Departament;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Departament obj = new Departament(1, "books");
+		System.out.println(obj);
+	}
+
+}
