51d40ebb2d2924302f4a15f3e6c7ade74d1581bf
helbertms <helbertms@live.com>
2020-06-16 20:18:36 -0300

    create metod findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,11 @@
 package application;
 
 
+import java.util.List;
+
 import model.dao.DaoFactury;
 import model.dao.SellerDao;
+import model.entites.Department;
 import model.entites.Seller;
 
 public class Program {
@@ -11,9 +14,16 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactury.createSellerDao();
 		
+		System.out.println(" ====== TESTE 1 - método findById ========");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
+		
+		System.out.println(" \n====== TESTE 2 - método findById ========");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
