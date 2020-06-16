c2fc9b678e10d22fb3c0829df7cfcea2f6e66ce2
helbertms <helbertms@live.com>
2020-06-16 20:28:53 -0300

    create metod findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,5 @@
 package application;
 
-
 import java.util.List;
 
 import model.dao.DaoFactury;
@@ -11,19 +10,26 @@ import model.entites.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		SellerDao sellerDao = DaoFactury.createSellerDao();
-		
+
 		System.out.println(" ====== TESTE 1 - método findById ========");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		
-		System.out.println(" \n====== TESTE 2 - método findById ========");
+
+		System.out.println("\n====== TESTE 2 - método findByDepartment ========");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+
+		System.out.println("\n====== TESTE 3 - método findByAll ========");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+
 	}
 
 }
